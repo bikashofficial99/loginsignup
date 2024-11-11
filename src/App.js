483987c391ad76e3226a75a08b8ee4d10453ce9d
Bikash Acharya <bikash.acharya614@gmail.com>
2024-11-11 21:43:15 +0545

    app

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import Form from "./components/Form";
 
 function App() {
   return (
-    <div className="text-5xl text-center">
-      Hello React
+    <div className="flex w-full h-screen">
+      <div className="w-full flex items-center justify-center lg:w-1/2">
       <Form />
+    </div>
+    <div className="hidden lg:flex h-full  w-1/2 items-center justify-center bg-gray-200 ">
+      <div className="w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full"/>
+
+      
+    </div>
     </div>
   );
 }
