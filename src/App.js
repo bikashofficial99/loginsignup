612342f74988edccb390eb12037eae05f1188843
Bikash Acharya <bikash.acharya614@gmail.com>
2024-11-14 21:43:40 +0545

    cn

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
       <div className="w-full flex items-center justify-center lg:w-1/2">
       <Form />
     </div>
-    <div className="hidden lg:flex h-full  w-1/2 items-center justify-center bg-gray-200 ">
+    <div className="hidden relative lg:flex h-full  w-1/2 items-center justify-center bg-gray-200 ">
       <div className="w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full"/>
-
+      <div className="w-full h-1/2 absolute bg-white/10 backdrop-blur"></div>
       
     </div>
     </div>
