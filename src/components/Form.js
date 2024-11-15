d767fa7a0422891c8ea82a898f50ecb96d0c15d1
Bikash Acharya <bikash.acharya614@gmail.com>
2024-11-15 10:58:23 +0545

    making form

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,8 +3,31 @@ import * as React from 'react';
 
 export default function Form(){
     return (
-        <div>
-            Hello Form
-        </div>
+        <div className='bg-white px-10 py-20 rounded-3xl border-2 broder-gray-200'>
+            <h1 className='text-5xl font-semibold'>Welcome Back</h1>
+            <p className='font-medium text-lg text-gray-500 mt-4'>Welcome back! Please enter your details.</p>
+            <div className='mt-8 '>
+                <div>
+                    <label className='text-lg font-medium'>Email</label>
+                    <input className='w-full border-2 border-gray-100 roundex-xl p-4 mt-4 bg-transparent'
+                    placeholder='Enter Your email'
+                     />
+                </div>
+                <div>
+                    <label className='text-lg font-medium'>Password</label>
+                    <input className='w-full border-2 border-gray-100 roundex-xl p-4 mt-4 bg-transparent'
+                    placeholder='Enter Your password'
+                     />
+                </div>
+               
+                <div>
+                    <div>
+                    <input type="checkbox" />
+                    <label className='ml-2 font-medium text-base'>Remember for 30 days</label>
+                </div>
+                <button className='font-medium text-base text-violet-500'>Forget pasword</button>
+            </div>
+            </div>
+            </div>
     )
 }
