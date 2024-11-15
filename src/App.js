e3a073d570a88ef0457775d2fabbff97530104e8
Bikash Acharya <bikash.acharya614@gmail.com>
2024-11-15 10:57:50 +0545

    app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <Form />
     </div>
     <div className="hidden relative lg:flex h-full  w-1/2 items-center justify-center bg-gray-200 ">
-      <div className="w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full"/>
+      <div className="w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full animate-bounce"/>
       <div className="w-full h-1/2 absolute bg-white/10 backdrop-blur"></div>
       
     </div>
