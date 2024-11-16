6af510da979d06cd3741ab0409e6dcfcfd5e03cf
Bikash Acharya <bikash.acharya614@gmail.com>
2024-11-16 21:35:04 +0545

    button

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,27 +6,32 @@ export default function Form(){
         <div className='bg-white px-10 py-20 rounded-3xl border-2 broder-gray-200'>
             <h1 className='text-5xl font-semibold'>Welcome Back</h1>
             <p className='font-medium text-lg text-gray-500 mt-4'>Welcome back! Please enter your details.</p>
-            <div className='mt-8 '>
+            <div className='mt-8'>
                 <div>
                     <label className='text-lg font-medium'>Email</label>
                     <input className='w-full border-2 border-gray-100 roundex-xl p-4 mt-4 bg-transparent'
                     placeholder='Enter Your email'
                      />
                 </div>
-                <div>
-                    <label className='text-lg font-medium'>Password</label>
+                <div className='mt-2'>
+                    <label className='text-lg font-medium '>Password</label>
                     <input className='w-full border-2 border-gray-100 roundex-xl p-4 mt-4 bg-transparent'
                     placeholder='Enter Your password'
                      />
                 </div>
                
-                <div>
+                <div className='mt-8 flex justify-between items-center'>
                     <div>
                     <input type="checkbox" />
                     <label className='ml-2 font-medium text-base'>Remember for 30 days</label>
                 </div>
                 <button className='font-medium text-base text-violet-500'>Forget Password</button>
             </div>
+            <div className='mt-8 flex flex-col gap-y-4'>
+            <button className='active:scale-[.98] active:duration-75 hover:scale-[1.01] transition-all py-4 rounded-xl bg-violet-500 text-white text-lg font-bold '>Sign in</button>
+            <button>Sign in with Goggle</button>
+            </div>
+            
             </div>
             </div>
     )
