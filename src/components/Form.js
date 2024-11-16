3caea32ec4b16a81ad29fa750d0cca488512fe80
Bikash Acharya <bikash.acharya614@gmail.com>
2024-11-16 21:44:20 +0545

    sign up and ui reactive

--- src/components/Form.js
+++ src/components/Form.js
@@ -25,15 +25,15 @@ export default function Form(){
                     <input type="checkbox" />
                     <label className='ml-2 font-medium text-base'>Remember for 30 days</label>
                 </div>
-                <button className='font-medium text-base text-violet-500'>Forget Password</button>
+                <button className='font-medium text-base text-violet-500 active:scale-[.98] active:duration-75 hover:scale-[1.01] transition-all'>Forget Password</button>
             </div>
             <div className='mt-8 flex flex-col gap-y-4'>
             <button className='active:scale-[.98] active:duration-75 hover:scale-[1.01] transition-all py-4 rounded-xl bg-violet-500 text-white text-lg font-bold '>Sign in</button>
-            <button>Sign in with Goggle</button>
+            <button className='active:scale-[.98] active:duration-75 hover:scale-[1.01] transition-all rounded-xl py-3 border-2 border-gray-100 items-center justify-center'>Sign in with Goggle</button>
             </div>
             <div className="mt-8 flex justify-center items-center">
-                <p className='font-medium text-base'>Don't have an account</p>
-                
+                <p className='font-medium text-base'>Don't have an account?</p>
+                <button className='text-violet-500 text-base font-medium ml-2 active:scale-[.98] active:duration-75 hover:scale-[1.01] transition-all'>Sign up</button>
             </div>
             
             </div>
